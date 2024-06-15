8c4b08396456dde8e42d6f7593f62054e16c70c9
CatKarsap <pelhjam@gmail.com>
2024-06-15 17:59:10 +0000

    cambios dev3

--- webapp/controller/Mainview.controller.js
+++ webapp/controller/Mainview.controller.js
@@ -1,12 +1,29 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
+/**
+ * 
+ * @param {Typeof sap.ui.core.mvc.controller} Controller 
+  */
 function (Controller) {
     "use strict";
 
     return Controller.extend("logaligroup.invoices.controller.Mainview", {
-        onInit: function () {
+        onInit : function () {
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView =this.getView();
+            oJSONModel.loadData("../model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "SelectionScreen");
+        },
 
+        onFilter : function (oEvent) {
+
+        },
+        onClearFilters : function () {
+
+            const oModelSelectionScreen =this.getView().getModel("SelectionScreen");
+            oModelSelectionScreen.setProperty("/ShipName", "");
+            oModelSelectionScreen.setProperty("/CountryKey", "");
         }
     });
 });
